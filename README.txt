40b660d4b8b906358c98876527773b5f8a6ce541
Mulia Sijabat <muliasijabat4@gmail.com>
2023-08-03 01:14:39 +0700

    first commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+ 👋 Hi, I’m @muliasijabat
+- 👀 I’m interested in Data Analys, Data Science and Machine Learning
+- 🌱 I’m currently learning about Data Euntusias
+- Fun fact. for the data i get from my course on coursera. and i want improve the project to get a dashboard on spreadsheet.
+- this project is still in the advanced process to create machine learning
---
+++ app.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import streamlit as st
+import streamlit.components.v1 as stc
+
+#import our app
+from ml_app import run_ml_app
+
+def main():
+    st.title("Main App")
+    # stc.html(html_temp)
+
+    menu = ["Home","Machine Learning"]
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    if choice == "Home":
+        st.subheader("Home")
+        # st.markdown(desc_temp, unsafe_allow_html=True)
+    elif choice == "Machine Learning":
+        st.subheader("Machine Learning Section")
+        run_ml_app()
+    
+if __name__ == '__main__':
+    main()
+    
